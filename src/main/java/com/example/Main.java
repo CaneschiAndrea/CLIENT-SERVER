5ebcd585fcf72f223710902d9138482cb51b4dd0
lai2-01 <lai2-01@itismeucci.com>
2023-10-03 09:06:34 +0200

    commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        Server server = new Server();
+        server.attendi();
+        server.comunica();
+    }
+}
